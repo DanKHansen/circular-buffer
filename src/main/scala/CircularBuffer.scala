6175500bebe095e8707e375dbd98c29205c45b58
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 15:48:34 +0200

    solution

--- src/main/scala/CircularBuffer.scala
+++ src/main/scala/CircularBuffer.scala
@@ -1,14 +1,23 @@
+import scala.collection.mutable.ListBuffer
+
 class EmptyBufferException() extends Exception {}
 
 class FullBufferException() extends Exception {}
 
-class CircularBuffer(var capacity: Int) {
+class CircularBuffer(val capacity: Int):
+   private val b = ListBuffer.empty[Int]
 
-  def write(value: Int) = ???
+   def write(value: Int): Unit = if capacity == b.size then throw new FullBufferException()
+   else b.append(value)
 
-  def read(): Int = ???
+   def read(): Int = if b.isEmpty then throw new EmptyBufferException()
+   else
+      val v = b.head
+      b.remove(0)
+      v
 
-  def overwrite(value: Int) = ???
+   def overwrite(value: Int): Unit =
+      if capacity <= b.size then b.remove(0) else ()
+      b.append(value)
 
-  def clear() = ???
-}
+   def clear(): Unit = b.clear()
