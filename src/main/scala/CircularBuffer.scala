b286f7b50fde648ae89383c82ec1ef79812e824e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 16:24:47 +0200

    refactor

--- src/main/scala/CircularBuffer.scala
+++ src/main/scala/CircularBuffer.scala
@@ -3,11 +3,11 @@ class EmptyBufferException() extends Exception {}
 class FullBufferException() extends Exception {}
 
 class CircularBuffer(val capacity: Int):
-   private val b = scala.collection.mutable.Queue[Int]()
+   private val b = scala.collection.mutable.ListBuffer.empty[Int]
 
    def write(value: Int): Unit = if capacity == b.size then throw FullBufferException() else b.append(value)
 
-   def read(): Int = if b.isEmpty then throw EmptyBufferException() else b.dequeue()
+   def read(): Int = if b.isEmpty then throw EmptyBufferException() else b.remove(0)
 
    def overwrite(value: Int): Unit =
       if capacity == b.size then read() else (); write(value)
